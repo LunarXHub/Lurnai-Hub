e51c46ec220f63fed2e7abc2a9b8c93ef0058b9c
HAVOC <limited1dangwr@gmail.com>
2025-04-17 12:35:03 -0500

    Update GameList.lua

--- Lurnai/Games/GameList.lua
+++ Lurnai/Games/GameList.lua
@@ -4,6 +4,7 @@ local Games = {
   [2753915549] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (First Sea)
   [4442272183] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (Second Sea)
   [7449423635] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (Third Sea)
+  [85896571713843] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/BubbleGum%20Simulator.lua", -- Bubble Gum Simulator (Soon)
 }
 
 return Games
