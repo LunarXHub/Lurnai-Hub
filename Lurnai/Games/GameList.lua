0ef16ede6b36b710d024f95271d30a2a3c990968
HAVOC <limited1dangwr@gmail.com>
2025-04-17 12:27:16 -0500

    Update GameList.lua

--- Lurnai/Games/GameList.lua
+++ Lurnai/Games/GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
   [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.Lua", -- Muscle Legends
-  [3101667897] = "NoUiYet", -- [3101667897] = -- Legends of Speed (SOON)
+  [3101667897] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Legends%20Of%20Speed.lua", -- Legends of Speed (SOON)
   [2753915549] = "NoUiYet", -- Blox Fruits (First Sea)
   [4442272183] = "NoUiYet", -- Blox Fruits (Second Sea)
   [7449423635] = "NoUiYet", -- Blox Fruits (Third Sea)
