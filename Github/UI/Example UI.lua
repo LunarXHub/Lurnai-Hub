35072fbbcdb1515ba99d52039277de8459a0770a
HAVOC <limited1dangwr@gmail.com>
2025-06-20 03:04:12 -0500

    Update Example UI.lua

--- Github/UI/Example UI.lua
+++ Github/UI/Example UI.lua
@@ -1,202 +1,261 @@
-local LibraryURL = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Github/UI/LurnaiHub_Library.lua"
-local Library, SaveManager, InterfaceManager = loadstring(game:HttpGet(LibraryURL))()
-print("Library loaded:", Library ~= nil)
-print("Available themes:", table.concat(Library.Themes, ", "))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HVX-Havoc/Testie/refs/heads/main/LurnaiHub.Luaaa"))()
 
-local WindowTitle = "Lurnai Hub | Grow A Garden"
-local WindowSubTitle = "By Havoc"
-local WindowTabWidth = 160
-local WindowSizeX = 380
-local WindowSizeY = 360
-local WindowAcrylic = true
-local WindowTheme = "LurnaiHub"
-
-local Window = Library:CreateWindow({
-    Title = WindowTitle,
-    SubTitle = WindowSubTitle,
-    TabWidth = WindowTabWidth,
-    Size = UDim2.fromOffset(WindowSizeX, WindowSizeY),
-    Acrylic = WindowAcrylic,
-    Theme = WindowTheme
+local Window = Fluent:CreateWindow({
+    Title = "Fluent " .. Fluent.Version,
+    SubTitle = "by dawid",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
-local HomeIcon = "house"
-local HomeTitle = "Home"
-local MainIcon = "gamepad-2"
-local MainTitle = "Main"
-local AutoIcon = "zap"
-local AutoTitle = "Auto Farm"
-local InventoryIcon = "package"
-local InventoryTitle = "Inventory"
-local ShopIcon = "shopping-cart"
-local ShopTitle = "Shop"
-local MiscIcon = "more-horizontal"
-local MiscTitle = "Misc"
-local SettingsIcon = "settings"
-local SettingsTitle = "Settings"
-local UISettingsIcon = "monitor"
-local UISettingsTitle = "UI Settings"
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
 
-local Home = Window:AddTab({
-    Title = HomeTitle,
-    Icon = HomeIcon
-})
+local Options = Fluent.Options
 
-local Main = Window:AddTab({
-    Title = MainTitle,
-    Icon = MainIcon
-})
+do
+    Fluent:Notify({
+        Title = "Notification",
+        Content = "This is a notification",
+        SubContent = "SubContent", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+    })
 
-local Automatically = Window:AddTab({
-    Title = AutoTitle,
-    Icon = AutoIcon
-})
 
-local Inventory = Window:AddTab({
-    Title = InventoryTitle,
-    Icon = InventoryIcon
-})
 
-local Shop = Window:AddTab({
-    Title = ShopTitle,
-    Icon = ShopIcon
-})
+    Tabs.Main:AddParagraph({
+        Title = "Paragraph",
+        Content = "This is a paragraph.\nSecond line!"
+    })
 
-local Misc = Window:AddTab({
-    Title = MiscTitle,
-    Icon = MiscIcon
-})
 
-local Settings = Window:AddTab({
-    Title = SettingsTitle,
-    Icon = SettingsIcon
-})
 
-local SettingsUI = Window:AddTab({
-    Title = UISettingsTitle,
-    Icon = UISettingsIcon
-})
-
-local MainSection = Main:AddSection("Main Features")
-
-MainSection:AddToggle("MainToggle", {
-    Title = "Enable Feature",
-    Description = "Toggle main functionality",
-    Default = false,
-    Callback = function(Value)
-        print("Main Toggle:", Value)
-    end
-})
-
-MainSection:AddSlider("WalkSpeedSlider", {
-    Title = "Walk Speed",
-    Description = "Adjust player walk speed",
-    Default = 16,
-    Min = 0,
-    Max = 100,
-    Rounding = 1,
-    Callback = function(Value)
-        if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+    Tabs.Main:AddButton({
+        Title = "Button",
+        Description = "Very important button",
+        Callback = function()
+            Window:Dialog({
+                Title = "Title",
+                Content = "This is a dialog",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            print("Confirmed the dialog.")
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                            print("Cancelled the dialog.")
+                        end
+                    }
+                }
+            })
         end
-    end
+    })
+
+
+
+    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
+
+    Toggle:OnChanged(function()
+        print("Toggle changed:", Options.MyToggle.Value)
+    end)
+
+    Options.MyToggle:SetValue(false)
+
+
+
+    local Slider = Tabs.Main:AddSlider("Slider", {
+        Title = "Slider",
+        Description = "This is a slider",
+        Default = 2,
+        Min = 0,
+        Max = 5,
+        Rounding = 1,
+        Callback = function(Value)
+            print("Slider was changed:", Value)
+        end
+    })
+
+    Slider:OnChanged(function(Value)
+        print("Slider changed:", Value)
+    end)
+
+    Slider:SetValue(3)
+
+
+
+    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
+        Title = "Dropdown",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = false,
+        Default = 1,
+    })
+
+    Dropdown:SetValue("four")
+
+    Dropdown:OnChanged(function(Value)
+        print("Dropdown changed:", Value)
+    end)
+
+
+
+    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
+        Title = "Dropdown",
+        Description = "You can select multiple values.",
+        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
+        Multi = true,
+        Default = {"seven", "twelve"},
+    })
+
+    MultiDropdown:SetValue({
+        three = true,
+        five = true,
+        seven = false
+    })
+
+    MultiDropdown:OnChanged(function(Value)
+        local Values = {}
+        for Value, State in next, Value do
+            table.insert(Values, Value)
+        end
+        print("Mutlidropdown changed:", table.concat(Values, ", "))
+    end)
+
+
+
+    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
+        Title = "Colorpicker",
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    Colorpicker:OnChanged(function()
+        print("Colorpicker changed:", Colorpicker.Value)
+    end)
+
+    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+
+
+
+    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
+        Title = "Colorpicker",
+        Description = "but you can change the transparency.",
+        Transparency = 0,
+        Default = Color3.fromRGB(96, 205, 255)
+    })
+
+    TColorpicker:OnChanged(function()
+        print(
+            "TColorpicker changed:", TColorpicker.Value,
+            "Transparency:", TColorpicker.Transparency
+        )
+    end)
+
+
+
+    local Keybind = Tabs.Main:AddKeybind("Keybind", {
+        Title = "KeyBind",
+        Mode = "Toggle", -- Always, Toggle, Hold
+        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
+
+        -- Occurs when the keybind is clicked, Value is `true`/`false`
+        Callback = function(Value)
+            print("Keybind clicked!", Value)
+        end,
+
+        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
+        ChangedCallback = function(New)
+            print("Keybind changed!", New)
+        end
+    })
+
+    -- OnClick is only fired when you press the keybind and the mode is Toggle
+    -- Otherwise, you will have to use Keybind:GetState()
+    Keybind:OnClick(function()
+        print("Keybind clicked:", Keybind:GetState())
+    end)
+
+    Keybind:OnChanged(function()
+        print("Keybind changed:", Keybind.Value)
+    end)
+
+    task.spawn(function()
+        while true do
+            wait(1)
+
+            -- example for checking if a keybind is being pressed
+            local state = Keybind:GetState()
+            if state then
+                print("Keybind is being held down")
+            end
+
+            if Fluent.Unloaded then break end
+        end
+    end)
+
+    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+
+
+    local Input = Tabs.Main:AddInput("Input", {
+        Title = "Input",
+        Default = "Default",
+        Placeholder = "Placeholder",
+        Numeric = false, -- Only allows numbers
+        Finished = false, -- Only calls callback when you press enter
+        Callback = function(Value)
+            print("Input changed:", Value)
+        end
+    })
+
+    Input:OnChanged(function()
+        print("Input updated:", Input.Value)
+    end)
+end
+
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
 })
 
-MainSection:AddParagraph({
-    Title = "Information",
-    Content = "This is a paragraph element that displays text information to users. It's useful for instructions or descriptions."
-})
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
 
-MainSection:AddKeybind("TestKeybind", {
-    Title = "Test Keybind",
-    Description = "Press a key!",
-    Default = "E",
-    Mode = "Toggle",
-    Callback = function(Value, Mode)
-        print("Keybind:", Value, "Mode:", Mode)
-    end
-})
-
-MainSection:AddButton({
-    Title = "Test Button",
-    Description = "Click me!",
-    Callback = function()
-        print("Button clicked!")
-    end
-})
-
-MainSection:AddToggle("TestToggle", {
-    Title = "Test Toggle",
-    Description = "Toggle me!",
-    Default = false,
-    Callback = function(Value)
-        print("Toggle:", Value)
-    end
-})
-
-MainSection:AddSlider("TestSlider", {
-    Title = "Test Slider",
-    Description = "Slide me!",
-    Default = 50,
-    Min = 0,
-    Max = 100,
-    Rounding = 1,
-    Callback = function(Value)
-        print("Slider:", Value)
-    end
-})
-
-MainSection:AddDropdown("TestDropdown", {
-    Title = "Test Dropdown",
-    Description = "Select an option!",
-    Values = {"Option 1", "Option 2", "Option 3"},
-    Multi = false,
-    Default = "Option 1",
-    Callback = function(Value)
-        print("Selected:", Value)
-    end
-})
-
-MainSection:AddDropdown("TestMultiDropdown", {
-    Title = "Multi Dropdown",
-    Description = "Select multiple!",
-    Values = {"Item A", "Item B", "Item C", "Item D"},
-    Multi = true,
-    Default = {"Item A"},
-    Callback = function(Value)
-        print("Multi-Selected:", table.concat(Value, ", "))
-    end
-})
-
-MainSection:AddInput("TestInput", {
-    Title = "Test Input",
-    Description = "Type something!",
-    Default = "Hello World",
-    Placeholder = "Enter text...",
-    Numeric = false,
-    Callback = function(Value)
-        print("Input:", Value)
-    end
-})
-
-MainSection:AddInput("TestNumericInput", {
-    Title = "Numeric Input",
-    Description = "Numbers only!",
-    Default = "123",
-    Placeholder = "Enter number...",
-    Numeric = true,
-    Callback = function(Value)
-        print("Number:", Value)
-    end
-})
-
-if SaveManager and InterfaceManager then
-    SaveManager:SetLibrary(Library)
-    InterfaceManager:SetLibrary(Library)
-    SaveManager:SetFolder("MyScript/Configs")
-    InterfaceManager:SetFolder("MyScript/Interface")
-
-    SaveManager:BuildConfigSection(Settings)
-    InterfaceManager:BuildInterfaceSection(SettingsUI)
+if not RunService:IsStudio() then
     SaveManager:LoadAutoloadConfig()
 end
