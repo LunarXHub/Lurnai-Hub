b8b623480e9fdf56d09440b802656952259e7cc6
HAVOC <limited1dangwr@gmail.com>
2025-06-20 22:02:05 -0500

    Update GameList.lua

--- Lurnai/Games/GameList.lua
+++ Lurnai/Games/GameList.lua
@@ -17,7 +17,7 @@ You may use this script **only for personal, non-commercial purposes.**
 Modification, redistribution, or claiming authorship of this script is **strictly prohibited** without explicit permission from the creator (LURNAI).
 ]]--
 local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.Lua", -- Muscle Legends
+  [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.lua", -- Muscle Legends
   [3101667897] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Legends%20Of%20Speed.lua", -- Legends of Speed (SOON)
   [2753915549] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (First Sea)
   [4442272183] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (Second Sea)
