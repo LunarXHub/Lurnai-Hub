66b1ac9f9305b1af4671fd79640f4fd82be42395
HAVOC <limited1dangwr@gmail.com>
2025-06-21 00:35:20 -0500

    Update GameList.lua

--- Lurnai/Games/GameList.lua
+++ Lurnai/Games/GameList.lua
@@ -16,6 +16,20 @@ LURNAI is the legal owner of this script and is protected by copyright law.
 You may use this script **only for personal, non-commercial purposes.**
 Modification, redistribution, or claiming authorship of this script is **strictly prohibited** without explicit permission from the creator (LURNAI).
 ]]--
+
+local url = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Github/UI/Button.lua"
+local success, scriptCode = pcall(function()
+    return game:HttpGet(url)
+end)
+if success and scriptCode then
+    local loaded = loadstring(scriptCode)
+    if type(loaded) == "function" then
+        loaded()
+    end
+else
+    warn("[LURNAI] Failed to load Button.lua from GitHub.")
+end
+
 local Games = {
   [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.luaa", -- Muscle Legends
   [3101667897] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Legends%20Of%20Speed.lua", -- Legends of Speed (SOON)
