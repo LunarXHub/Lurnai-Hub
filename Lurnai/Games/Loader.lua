64a6dbe5812725f801203bd55f9239446f005ec6
HAVOC <limited1dangwr@gmail.com>
2025-06-21 00:42:51 -0500

    Update Loader.lua

--- Lurnai/Games/Loader.lua
+++ Lurnai/Games/Loader.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/GameList.Lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
