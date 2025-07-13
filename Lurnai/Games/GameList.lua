63d9af1478907a64810941651ca9b1950c37de05
HAVOC <limited1dangwr@gmail.com>
2025-07-12 23:05:18 -0500

    Update and rename GameList.Lua to GameList.lua

--- Lurnai/Games/GameList.lua
+++ Lurnai/Games/GameList.lua
@@ -31,7 +31,7 @@ else
 end
 
 local Games = {
-  [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.luaa", -- Muscle Legends
+  [3623096087] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Muscle%20Legends.luaaa", -- Muscle Legends
   [3101667897] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Legends%20Of%20Speed.lua", -- Legends of Speed (SOON)
   [2753915549] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (First Sea)
   [4442272183] = "https://raw.githubusercontent.com/HVX-Havoc/Lurnai-Hub/refs/heads/main/Lurnai/Games/Blox%20Fruits.lua", -- Blox Fruits (Second Sea)
