d304bb2cdf7804927bc24b85536963debcb9a868
HAVOC <limited1dangwr@gmail.com>
2025-07-13 22:19:38 -0500

    Create  Pets and Auras.lua

---
+++ Lurnai/Data Game/Muscle Legends/Pets and Auras.lua
@@ -0,0 +1,81 @@
+Basic Pets:
+  Blue Birdie
+  Blue Bunny
+  Orange Hedgehog
+  Red Kitty
+  Silver Dog
+
+Advanced Pets:
+  Dark Golem
+  Dark Vampy
+  Green Butterfly
+  Yellow Butterfly
+
+Rare Pets:
+  Crimson Falcon
+  Eternal Strike Leviathan
+  Frostwave Legends Penguin
+  Orange Pegasus
+  Phantom Genesis Dragon
+  Purple Dragon
+  Purple Falcon
+  Red Dragon
+  White Pegasus
+
+Epic Pets:
+  Blue Firecaster
+  Blue Pheonix
+  Dark Legends Manticore
+  Golden Pheonix
+  Golden Pheonix
+  Golden Viking
+  Green Firecaster
+  Lightning Strike Phantom
+  Red Firecaster
+  Ultimate Supernova Pegasus
+  White Pheonix
+
+Unique Pets:
+  Aether Spirit Bunny
+  Cybernetic Showdown Dragon
+  Darkstar Hunter
+  Gold Warrior
+  Infernal Dragon
+  Magic Butterfly
+  Muscle Sensei
+  Neon Guardian
+  Ultra Birdie
+
+Basic Auras:
+  Blue Aura
+  Green Aura
+  Purple Aura
+  Red Aura
+  Yellow Aura
+
+Advanced Auras:
+  Electro
+  Purple Nova
+
+Rare Auras:
+  Astral Electro
+  Dark Electro
+  Power Lightning
+  Supernova
+  Ultra Inferno
+  Ultra Mirage
+
+Epic Auras:
+  Azure Tundra
+  Enchanted Mirage
+  Grand Supernova
+  Lightning
+  Unstable Mirage
+
+Unique Auras:
+  Dark Lightning
+  Dark Storm
+  Entropic Blast
+  Eternal Megastrike
+  Inferno
+  Muscle King
