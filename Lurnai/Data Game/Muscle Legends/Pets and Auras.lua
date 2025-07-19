fd80f75b59ac662c80e0d0f70c7de852385f2ad4
HAVOC <limited1dangwr@gmail.com>
2025-07-18 23:31:16 -0500

    Update Pets and Auras.lua

--- Lurnai/Data Game/Muscle Legends/Pets and Auras.lua
+++ Lurnai/Data Game/Muscle Legends/Pets and Auras.lua
@@ -1,81 +1,458 @@
-Basic Pets:
-  Blue Birdie
-  Blue Bunny
-  Orange Hedgehog
-  Red Kitty
-  Silver Dog
-
-Advanced Pets:
-  Dark Golem
-  Dark Vampy
-  Green Butterfly
-  Yellow Butterfly
-
-Rare Pets:
-  Crimson Falcon
-  Eternal Strike Leviathan
-  Frostwave Legends Penguin
-  Orange Pegasus
-  Phantom Genesis Dragon
-  Purple Dragon
-  Purple Falcon
-  Red Dragon
-  White Pegasus
-
-Epic Pets:
-  Blue Firecaster
-  Blue Pheonix
-  Dark Legends Manticore
-  Golden Pheonix
-  Golden Pheonix
-  Golden Viking
-  Green Firecaster
-  Lightning Strike Phantom
-  Red Firecaster
-  Ultimate Supernova Pegasus
-  White Pheonix
-
-Unique Pets:
-  Aether Spirit Bunny
-  Cybernetic Showdown Dragon
-  Darkstar Hunter
-  Gold Warrior
-  Infernal Dragon
-  Magic Butterfly
-  Muscle Sensei
-  Neon Guardian
-  Ultra Birdie
-
-Basic Auras:
-  Blue Aura
-  Green Aura
-  Purple Aura
-  Red Aura
-  Yellow Aura
-
-Advanced Auras:
-  Electro
-  Purple Nova
-
-Rare Auras:
-  Astral Electro
-  Dark Electro
-  Power Lightning
-  Supernova
-  Ultra Inferno
-  Ultra Mirage
-
-Epic Auras:
-  Azure Tundra
-  Enchanted Mirage
-  Grand Supernova
-  Lightning
-  Unstable Mirage
-
-Unique Auras:
-  Dark Lightning
-  Dark Storm
-  Entropic Blast
-  Eternal Megastrike
-  Inferno
-  Muscle King
+{
+  "Blue Crystal": [
+    {
+      "chance": 15,
+      "image": "rbxassetid://2880554334",
+      "name": "Blue Birdie",
+      "rarity": "Basic"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2877421636",
+      "name": "Orange Hedgehog",
+      "rarity": "Basic"
+    },
+    {
+      "chance": 25,
+      "image": "Question Mark",
+      "name": "Basic Aura",
+      "rarity": "Basic"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://2880471779",
+      "name": "Red Kitty",
+      "rarity": "Basic"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://2880554565",
+      "name": "Dark Vampy",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://2880465323",
+      "name": "Blue Bunny",
+      "rarity": "Basic"
+    }
+  ],
+  "Green Crystal": [
+    {
+      "chance": 20,
+      "image": "rbxassetid://2880471833",
+      "name": "Silver Dog",
+      "rarity": "Basic"
+    },
+    {
+      "chance": 20,
+      "image": "Question Mark",
+      "name": "Advanced Aura",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2896963482",
+      "name": "Dark Golem",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://2880640009",
+      "name": "Green Butterfly",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://2896970781",
+      "name": "Crimson Falcon",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 15,
+      "image": "Question Mark",
+      "name": "Basic Aura",
+      "rarity": "Basic"
+    }
+  ],
+  "Frost Crystal": [
+    {
+      "chance": 20,
+      "image": "Question Mark",
+      "name": "Advanced Aura",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://2880739585",
+      "name": "Yellow Butterfly",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2880736201",
+      "name": "Purple Dragon",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 5,
+      "image": "rbxassetid://2967187307",
+      "name": "Blue Pheonix",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 15,
+      "image": "Question Mark",
+      "name": "Rare Aura",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://2896966544",
+      "name": "Orange Pegasus",
+      "rarity": "Rare"
+    }
+  ],
+  "Mythical Crystal": [
+    {
+      "chance": 20,
+      "image": "Question Mark",
+      "name": "Rare Aura",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 15,
+      "image": "Question Mark",
+      "name": "Epic Aura",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2896966605",
+      "name": "Purple Falcon",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://2880736333",
+      "name": "Red Dragon",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://2967159618",
+      "name": "Blue Firecaster",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://2967191408",
+      "name": "Golden Pheonix",
+      "rarity": "Epic"
+    }
+  ],
+  "Inferno Crystal": [
+    {
+      "chance": 25,
+      "image": "Question Mark",
+      "name": "Epic Aura",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 5,
+      "image": "Question Mark",
+      "name": "Unique Aura",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://2967160108",
+      "name": "Red Firecaster",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 5,
+      "image": "rbxassetid://3024078491",
+      "name": "Infernal Dragon",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2896974406",
+      "name": "White Pegasus",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://2967191408",
+      "name": "Golden Pheonix",
+      "rarity": "Epic"
+    }
+  ],
+  "Legends Crystal": [
+    {
+      "chance": 25,
+      "image": "Question Mark",
+      "name": "Epic Aura",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 10,
+      "image": "Question Mark",
+      "name": "Unique Aura",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 5,
+      "image": "rbxassetid://3024081423",
+      "name": "Ultra Birdie",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://3024078574",
+      "name": "Magic Butterfly",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://2967159769",
+      "name": "Green Firecaster",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://2967188752",
+      "name": "White Pheonix",
+      "rarity": "Epic"
+    }
+  ],
+  "Muscle Elite Crystal": [
+    {
+      "chance": 30,
+      "image": "rbxassetid://4572790315",
+      "name": "Frostwave Legends Penguin",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://4472944231",
+      "name": "Phantom Genesis Dragon",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://4440223019",
+      "name": "Dark Legends Manticore",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://4493755085",
+      "name": "Ultimate Supernova Pegasus",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 9,
+      "image": "rbxassetid://4690100727",
+      "name": "Aether Spirit Bunny",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 1,
+      "image": "rbxassetid://4739873228",
+      "name": "Cybernetic Showdown Dragon",
+      "rarity": "Unique"
+    }
+  ],
+  "Galaxy Oracle Crystal": [
+    {
+      "chance": 20,
+      "image": "rbxassetid://4493756669",
+      "name": "Eternal Strike Leviathan",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 15,
+      "image": "http://www.roblox.com/asset/?id=4845530202",
+      "name": "Lightning Strike Phantom",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 3,
+      "image": "rbxassetid://4776504040",
+      "name": "Darkstar Hunter",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 2,
+      "image": "rbxassetid://3111907185",
+      "name": "Muscle King Aura",
+      "rarity": "Unique",
+      "color": [0,0,0]
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://3258517083",
+      "name": "Azure Tundra Aura",
+      "rarity": "Epic",
+      "color": [0.666667,0.333333,1]
+    },
+    {
+      "chance": 35,
+      "image": "rbxassetid://3111889131",
+      "name": "Ultra Inferno Aura",
+      "rarity": "Rare",
+      "color": [0,0.666667,1]
+    }
+  ],
+  "Battle Legends Crystal": [
+    {
+      "chance": 30,
+      "image": "rbxassetid://9019941269",
+      "name": "Alien Girl",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://9019941040",
+      "name": "Green-Fly",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://9019941456",
+      "name": "Ice Ice",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://9019942465",
+      "name": "Slitherman",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 9,
+      "image": "rbxassetid://9019942170",
+      "name": "Hank",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 1,
+      "image": "rbxassetid://9020270344",
+      "name": "Gold Warrior",
+      "rarity": "Unique"
+    }
+  ],
+  "Dark Nebula Crystal": [
+    {
+      "chance": 15,
+      "image": "rbxassetid://9019941827",
+      "name": "Mask Of Gold",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 25,
+      "image": "rbxassetid://9019941997",
+      "name": "Mr. Serious",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 30,
+      "image": "rbxassetid://9019942702",
+      "name": "Smiley",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://9019940744",
+      "name": "A. Gnatomy",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://9019969180",
+      "name": "Sir Flame",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 5,
+      "image": "rbxassetid://9019941673",
+      "name": "Sky Hawk",
+      "rarity": "Unique"
+    }
+  ],
+  "Sky Eclipse Crystal": [
+    {
+      "chance": 30,
+      "image": "rbxassetid://9353695214",
+      "name": "Chops",
+      "rarity": "Advanced"
+    },
+    {
+      "chance": 30,
+      "image": "rbxassetid://9353575434",
+      "name": "Boy Wonder",
+      "rarity": "Rare"
+    },
+    {
+      "chance": 20,
+      "image": "rbxassetid://9360057319",
+      "name": "Masked Marksman",
+      "rarity": "Epic"
+    },
+    {
+      "chance": 5,
+      "image": "rbxassetid://9353429516",
+      "name": "Cool Guy Larry",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://9360109618",
+      "name": "Screaming Bones",
+      "rarity": "Unique"
+    }
+  ],
+  "Jungle Crystal": [
+    {
+      "chance": 5,
+      "image": "rbxassetid://3111889131",
+      "name": "Entropic Blast Aura",
+      "rarity": "Unique",
+      "color": [0,0.666667,1]
+    },
+    {
+      "chance": 10,
+      "image": "rbxassetid://108795756988039",
+      "name": "Muscle Sensei",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 24,
+      "image": "rbxassetid://3111907185",
+      "name": "Grand Supernova Aura",
+      "rarity": "Epic",
+      "color": [0,0,0]
+    },
+    {
+      "chance": 1,
+      "image": "rbxassetid://129419449488769",
+      "name": "Neon Guardian",
+      "rarity": "Unique"
+    },
+    {
+      "chance": 15,
+      "image": "rbxassetid://3258517083",
+      "name": "Eternal Megastrike Aura",
+      "rarity": "Unique",
+      "color": [0.666667,0,1]
+    },
+    {
+      "chance": 45,
+      "image": "rbxassetid://90520953823917",
+      "name": "Golden Viking",
+      "rarity": "Epic"
+    }
+  ]
+}
